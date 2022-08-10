e24d8630565eb4339fd46332f145241d63b23d6b
Erickcufe <erickcufe94@gmail.com>
2022-08-09 20:28:43 -0500

    .

---
+++ R/contact_matrices.R
@@ -0,0 +1,5 @@
+# Falta obtener las coordenadas de cada GRanges para la matriz de contactos.
+library(HiCBricks)
+
+out_dir <- file.path("data_tmp", "mcool_to_Brick_test")
+My_BrickContainer <- load_BrickContainer(project_dir = out_dir)
